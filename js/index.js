56c6c04fa9b784f29c515c08dfc6041bc29b21d6
Fernando Vieira <nandovieira2@yahoo.com.br>
2020-01-28 19:28:31 -0300

    Alteração de background dos ingressos e alteração do link da pagina inicial

--- js/index.js
+++ js/index.js
@@ -28,10 +28,31 @@ function to_event() {
     show_event();
 }
 
+
+function to_ticket() {
+    const now = document.getElementById('home');
+    now.classList.remove('active');
+    now.classList.remove('show');
+    document.getElementById('ingressos').setAttribute('class', 'tb-pane active show');
+
+    const link = document.getElementById('home-tab');
+    link.classList.remove('active');
+    link.classList.remove('show');
+    document.getElementById('home-tab').setAttribute('aria-selected', 'false');
+    document.getElementById('ingressos-tab').setAttribute('aria-selected', 'true');
+    document.getElementById('ingressos-tab').setAttribute('class', 'nav-link active show');
+
+    show_ticket();
+}
+
 function hide_event() {
     document.getElementById('evento').style.display='none'; 
 }
 
 function show_event() {
     document.getElementById('evento').style.display='block'; 
+}
+
+function show_ticket() {
+    document.getElementById('ingressos').style.display='block'; 
 }
